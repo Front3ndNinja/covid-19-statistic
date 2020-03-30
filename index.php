d70be56d7a4daefd8830cbb9e5102cd932c3505d
Fardin Ahmed <niloy98.it@gmail.com>
2020-03-31 02:08:53 +0600

    minor fix

--- index.php
+++ index.php
@@ -52,26 +52,52 @@ $data = json_decode($result, true);
                 </div>
             </div>
         </section>
+
         <section>
             <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h3>করোনা ভাইরাস প্রতিরোধের উপায়</h3>
-                        <ul>
-                            <li> বার বার সাবান ও পানি দিয়ে হাত ধুবেন (প্রতিবারে ২০ সেকেন্ডের বেশি) </li>
-                            <li> প্রয়োজনে স্যানিটাইজার ব্যবহার করতে পারেন </li>
-                            <li> জরুরি প্রয়োজন ব্যতিরেকে জনসমাগম এড়িয়ে চলুন, বেশিরভাগ সময় বাড়িতে থাকার চেষ্টা করুন </li>
-                        </ul>
-                    </div>
-                    <div class="col-md-12">
-                        <div id="quiz"></div>
-                        <div id="results"></div>
-                        <button id="submit">Get Results</button>
+                <div class = "row">
+                    <div class="col-md-12 text-center">
+                    <h4 class="text-danger margin-top-bottom">করোনা ঠেকাতে বাসাতে থাকুন</h3>
+
+                        <img class="img-fluid " src="img/corona.jpg" alt="">
                     </div>
                 </div>
             </div>
         </section>
-        <footer>
+
+        <section class="safe">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                    <h3 class="text-center margin-top-bottom">করোনা ভাইরাস প্রতিরোধের উপায়</h3>
+                        <ul>
+                            <li> ১. বার বার সাবান ও পানি দিয়ে হাত ধুবেন (প্রতিবারে ২০ সেকেন্ডের বেশি)  <a href="https://www.youtube.com/watch?v=L-QWAi6n1Bk">হাত ধোয়ার সঠিক নিয়ম শিখুন</a></li>
+                            <li> ২. প্রয়োজনে স্যানিটাইজার ব্যবহার করতে পারেন </li>
+                            <li> ৩. জরুরি প্রয়োজন ব্যতিরেকে জনসমাগম এড়িয়ে চলুন, বেশিরভাগ সময় বাড়িতে থাকার চেষ্টা করুন </li>
+                        </ul>
+                    </div>
+                    
+                    <div class="col-md-12 text-center">
+                    <h3 class="margin-top-bottom"> আপনি কি করোনা ভাইরাসে ( কোভিড-১৯) আক্রান্ত ? </h3>
+                    <button class="submit"onclick="myFunction()">পরীক্ষা করুন</button>
+                    </div>
+                    <div class="col-md-12 quiz" id = "quizHide">
+                        
+                        <div id="quiz"></div>
+                        <div id="results">
+                            <div class="test">
+                                
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <button class="submit" id="submit">ফলাফল</button>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer class="margin-top-bottom">
             <div class="container">
                 <div class="row">
                     <p class="card-text"> <small>
@@ -85,6 +111,16 @@ $data = json_decode($result, true);
         <script src="js/jquery.min.js"></script>
         <script src="js/popper.min.js"></script>
         <script src="js/quiz.js"></script>
+        <script>
+            function myFunction() {
+  var x = document.getElementById("quizHide");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+} 
+        </script>
         <script src="js/bootstrap.min.js"></script>
     </body>
 
