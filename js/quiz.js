aad4b06a2a906d43acff9529ef9b92cabbf002a8
Fardin Ahmed <niloy98.it@gmail.com>
2020-03-31 02:08:38 +0600

    added number in question & fixed answer formatting

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,5 @@
 var myQuestions = [{
-        question: "আপনার কাশি আছে কিনা?",
+        question: "১. আপনার কাশি আছে কিনা?",
         answers: {
             হ্যাঁ: '',
             না: '',
@@ -8,7 +8,7 @@ var myQuestions = [{
     },
 
     {
-        question: "ঠান্ডা লেগেছে কিনা?",
+        question: "২. ঠান্ডা লেগেছে কিনা?",
         answers: {
             হ্যাঁ: '',
             না: '',
@@ -17,7 +17,7 @@ var myQuestions = [{
     },
 
     {
-        question: "ডায়রিয়া হচ্ছে কিনা?",
+        question: "৩. ডায়রিয়া হচ্ছে কিনা?",
         answers: {
             হ্যাঁ: '',
             না: '',
@@ -26,7 +26,7 @@ var myQuestions = [{
     },
 
     {
-        question: "গলায় ক্ষত বা ঘা হয়েছে কিনা?",
+        question: "৪. গলায় ক্ষত বা ঘা হয়েছে কিনা?",
         answers: {
             হ্যাঁ: '',
             না: '',
@@ -35,14 +35,14 @@ var myQuestions = [{
     },
 
     {
-        question: "পেশি বা শরীরে ব্যাথা আছে কিনা?",
+        question: "৫. পেশি বা শরীরে ব্যাথা আছে কিনা?",
         answers: {
             হ্যাঁ: '',
             না: '',
         },
         correctAnswer: 'হ্যাঁ'
     }, {
-        question: "মাথা ব্যাথা আছে কিনা?",
+        question: "৬. মাথা ব্যাথা আছে কিনা?",
         answers: {
             হ্যাঁ: '',
             না: '',
@@ -51,14 +51,14 @@ var myQuestions = [{
     },
 
     {
-        question: "আপনার জ্বর ১০০ ডিগ্রী ফারেনহাইট বা তার উপরে কিনা?",
+        question: "৭. আপনার জ্বর ১০০ ডিগ্রী ফারেনহাইট বা তার উপরে কিনা?",
         answers: {
             হ্যাঁ: '',
             না: '',
         },
         correctAnswer: 'হ্যাঁ'
     }, {
-        question: "শ্বাস নিতে কষ্ট হয় কিনা?",
+        question: "৮. শ্বাস নিতে কষ্ট হয় কিনা?",
         answers: {
             হ্যাঁ: '',
             না: '',
@@ -66,7 +66,7 @@ var myQuestions = [{
         correctAnswer: 'হ্যাঁ',
         weight: '2'
     }, {
-        question: "আপনার অবসাদ/ক্লান্তি লাগে কিনা?",
+        question: "৯. আপনার অবসাদ/ক্লান্তি লাগে কিনা?",
         answers: {
             হ্যাঁ: '',
             না: '',
@@ -74,7 +74,15 @@ var myQuestions = [{
         correctAnswer: 'হ্যাঁ',
         weight: '2'
     }, {
-        question: "গত ১৪ দিনে কোথাও ভ্রমন করেছেন কিনা?",
+        question: "১০. গত ১৪ দিনে কোথাও ভ্রমন করেছেন কিনা?",
+        answers: {
+            হ্যাঁ: '',
+            না: '',
+        },
+        correctAnswer: 'হ্যাঁ',
+        
+    }, {
+        question: "১১. করোনা আক্রান্ত এলাকায় ভ্রমন করেছেন কিনা?",
         answers: {
             হ্যাঁ: '',
             না: '',
@@ -82,15 +90,7 @@ var myQuestions = [{
         correctAnswer: 'হ্যাঁ',
         weight: '3'
     }, {
-        question: "করোনা আক্রান্ত এলাকায় ভ্রমন করেছেন কিনা?",
-        answers: {
-            হ্যাঁ: '',
-            না: '',
-        },
-        correctAnswer: 'হ্যাঁ',
-        weight: '3'
-    }, {
-        question: " করোনা আক্রান্ত কোন ব্যাক্তির সংস্পর্শে গিয়েছেন কিনা?",
+        question: "১২.করোনা আক্রান্ত কোন ব্যাক্তির সংস্পর্শে গিয়েছেন কিনা?",
         answers: {
             হ্যাঁ: '',
             না: '',
@@ -116,7 +116,7 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
             // for each available answer...
             for (letter in questions[i].answers) {
                 // ...add an html radio button
-                answers.push('<label style="margin-right: 40px;">' + '<input type="radio" name="question' + i + '" value="' + letter + '">' + letter + ': ' + questions[i].answers[letter] + '</label>');
+                answers.push('<label style="margin-right: 40px;">' + '<input type="radio" name="question' + i + '" value="' + letter + '">' + letter + questions[i].answers[letter] + '</label>');
             }
             // add this question and its answers to the output
             output.push('<div class="question">' + questions[i].question + '</div>' + '<div class="answers">' + answers.join('') + '</div>');
@@ -152,14 +152,23 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
                 }
             }
         }
-        if (numCorrect > 10) {
-            resultsContainer.innerHTML = 'অতিসত্তর মোবাইলে আপনার নিকটবর্তী হাসপাতাল বা ৩৩৩, ১৬২৬৩, ১০৬৫৫ অথবা আইইডিসিআর এর হটলাইন নাম্বারে যোগাযোগ করবেন দেরি না করে শ্বাসকষ্টের চিকিৎসার জন্যে হাসপাতালে যান। হাসপাতালে যাওয়ার সময় অবস্যই মাস্ক পরিধান করবেন এবং গণপরিবহন বা ভীড় এড়িয়ে চলবেন।যাতাযাতের সময় আপনার সাথে শুধু একজন সঙ্গী থাকতে পারবে তবে তাঁর সাথে নিরাপদ দূরত্ব ( ৩ ফুট ) বজায় রাখতে হবে। ';
-        } else if (dangerLevel == true) {
-            resultsContainer.innerHTML = 'অতিসত্তর মোবাইলে আপনার নিকটবর্তী হাসপাতাল বা ৩৩৩, ১৬২৬৩, ১০৬৫৫ অথবা আইইডিসিআর এর হটলাইন নাম্বারে যোগাযোগ করবেন দেরি না করে শ্বাসকষ্টের চিকিৎসার জন্যে হাসপাতালে যান। হাসপাতালে যাওয়ার সময় অবস্যই মাস্ক পরিধান করবেন এবং গণপরিবহন বা ভীড় এড়িয়ে চলবেন।যাতাযাতের সময় আপনার সাথে শুধু একজন সঙ্গী থাকতে পারবে তবে তাঁর সাথে নিরাপদ দূরত্ব ( ৩ ফুট ) বজায় রাখতে হবে। ';
-        } else {
-            resultsContainer.innerHTML = ' এইমুহূর্তে আপনার করোনা ভাইরাসের ঝুঁকি খুব কম';
+        
+
+        if(answerContainers.length>11){
+          
+            if (numCorrect > 10) {
+                resultsContainer.innerHTML = ' <h4> অতিসত্তর মোবাইলে আপনার নিকটবর্তী হাসপাতাল বা <span class="text-danger">৩৩৩, ১৬২৬৩, ১০৬৫৫</span> অথবা আইইডিসিআর এর হটলাইন নাম্বারে যোগাযোগ করবেন। <br> হাসপাতালে যাওয়ার সময় অবস্যই মাস্ক পরিধান করবেন এবং গণপরিবহন বা ভীড় এড়িয়ে চলবেন। <br>যাতাযাতের সময় আপনার সাথে শুধু একজন সঙ্গী থাকতে পারবে তবে তাঁর সাথে নিরাপদ দূরত্ব ( ৩ ফুট ) বজায় রাখতে হবে। </h4> ';
+            } else if (dangerLevel == true) {
+                resultsContainer.innerHTML = ' <h4> অতিসত্তর মোবাইলে আপনার নিকটবর্তী হাসপাতাল বা <span class="text-danger">৩৩৩, ১৬২৬৩, ১০৬৫৫</span> অথবা আইইডিসিআর এর হটলাইন নাম্বারে যোগাযোগ করবেন। <br> হাসপাতালে যাওয়ার সময় অবস্যই মাস্ক পরিধান করবেন এবং গণপরিবহন বা ভীড় এড়িয়ে চলবেন। <br>যাতাযাতের সময় আপনার সাথে শুধু একজন সঙ্গী থাকতে পারবে তবে তাঁর সাথে নিরাপদ দূরত্ব ( ৩ ফুট ) বজায় রাখতে হবে। </h4> ';
+            } else {
+                resultsContainer.innerHTML = ' <h4 class="text-center text-dangerless">এইমুহূর্তে আপনার করোনা ভাইরাসের ঝুঁকি খুব কম<h4>';
+            }
         }
-        console.log(numCorrect)
+        else{
+            
+        }
+       
+        
     }
     // show questions right away
     showQuestions(questions, quizContainer);
